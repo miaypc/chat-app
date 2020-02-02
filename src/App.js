f09ef4f2b062875641bbd7a0e7f863631e124a35
miaypc <55692126+miaypc@users.noreply.github.com>
2020-02-02 15:11:50 +0100

    quest2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from "react";
 import "./styles.css";
 import UserInfo from "./components/Contact.js";
+import UserData from "./components/ContactList.js";
 
 export default function App() {
   return (
     <div className="App">
-      <UserInfo
-        avatar="https://randomuser.me/api/portraits/women/69.jpg"
-        name="Beverley Matthews"
-        text="online"
-      />
-      <UserInfo
-        avatar="https://randomuser.me/api/portraits/men/22.jpg"
-        name="Jeffery Hayes"
-        text="offline"
-      />
-      <UserInfo
-        avatar="https://randomuser.me/api/portraits/women/51.jpg"
-        name="Lily Baker"
-        text="online"
-      />
+      {UserData.map(element => {
+        return (
+          <UserInfo
+            avatar={element.avatar}
+            name={element.name}
+            text={element.text}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const UserData = [
+  {
+    name: "Phyllis Jacobs",
+    avatar: "https://randomuser.me/api/portraits/women/65.jpg",
+    text: "online"
+  },
+  {
+    name: "Sylvia Wright",
+    avatar: "https://randomuser.me/api/portraits/women/90.jpg",
+    text: "offline"
+  },
+  {
+    name: "Nelson Moore",
+    avatar: "https://randomuser.me/api/portraits/men/78.jpg",
+    text: "online"
+  },
+  {
+    name: "Elmer Palmer",
+    avatar: "https://randomuser.me/api/portraits/men/59.jpg",
+    text: "online"
+  },
+  {
+    name: "Sara Allen",
+    avatar: "https://randomuser.me/api/portraits/women/12.jpg",
+    text: "offline"
+  }
+];
+export default UserData;
